fec6e26d8fec7af89b47c1a8765c12fd768e13b8
Umidbek Karimov <uma.karimov@gmail.com>
2018-09-26 15:17:23 +0500

    chore(jest.config.js): Setup `coverageThreshold`.

--- jest.config.js
+++ jest.config.js
@@ -6,4 +6,13 @@ module.exports = {
   setupFiles: ["<rootDir>/config/setupTests.js"],
   snapshotSerializers: ["enzyme-to-json/serializer"],
   testMatch: ["**/__tests__/**/*.spec.ts?(x)"],
+  collectCoverageFrom: ["src/**/*.ts?(x)", "!src/**/__tests__/**/*"],
+  coverageThreshold: {
+    global: {
+      statements: 95,
+      branches: 95,
+      functions: 95,
+      lines: 95,
+    },
+  },
 };
