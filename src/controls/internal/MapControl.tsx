5409b7fbfd6a8bacd10154bcc54504cefabe3edb
Umidbek Karimov <uma.karimov@gmail.com>
2018-09-26 13:35:22 +0500

    refactor(MapControl): Use `MapComponent`.

--- src/controls/internal/MapControl.tsx
+++ src/controls/internal/MapControl.tsx
@@ -1,8 +1,8 @@
 import * as React from "react";
 
-import { GoogleMapContextConsumer } from "../../google-map-context/GoogleMapContext";
+import { MapComponent } from "../../google-map-component/MapComponent";
+import { GoogleMapContext } from "../../google-map-context/GoogleMapContext";
 import { isDeepEqual } from "../../internal/DataUtils";
-import { ValueSpy } from "../../internal/ValueSpy";
 
 export type MapControlPosition =
   | "BOTTOM_CENTER"
@@ -23,54 +23,40 @@ interface Control<N extends keyof google.maps.MapOptions, O = never> {
   options?: O;
 }
 
+type ControlVariant =
+  | Control<"fullscreenControl", google.maps.FullscreenControlOptions>
+  | Control<"mapTypeControl", google.maps.MapTypeControlOptions>
+  | Control<"rotateControl", google.maps.RotateControlOptions>
+  | Control<"scaleControl">
+  | Control<"streetViewControl", google.maps.StreetViewControlOptions>
+  | Control<"zoomControl", google.maps.ZoomControlOptions>;
+
 export interface MapControlProps {
-  createControl: (
-    maps: typeof google.maps,
-  ) =>
-    | Control<"fullscreenControl", google.maps.FullscreenControlOptions>
-    | Control<"mapTypeControl", google.maps.MapTypeControlOptions>
-    | Control<"rotateControl", google.maps.RotateControlOptions>
-    | Control<"scaleControl">
-    | Control<"streetViewControl", google.maps.StreetViewControlOptions>
-    | Control<"zoomControl", google.maps.ZoomControlOptions>;
+  createControl: (maps: typeof google.maps) => ControlVariant;
+}
+
+function createMapOptions(visible: boolean, control: ControlVariant) {
+  return {
+    [control.name]: visible,
+    [`${control.name}Options`]: !visible ? undefined : control.options,
+  };
 }
 
 export function MapControl({ createControl }: MapControlProps) {
   return (
-    <GoogleMapContextConsumer>
-      {({ map, maps }) => {
-        if (!map || !maps) {
-          return null;
-        }
-
-        const control = createControl(maps);
-
-        const setOptions = (visible: boolean) => {
-          map.setOptions({
-            [control.name]: visible,
-            [`${control.name}Options`]:
-              !visible || control.name === "scaleControl"
-                ? undefined
-                : control.options,
-          });
-        };
-
-        const updateOptions = () => setOptions(true);
-        const removeOptions = () => setOptions(false);
-
-        return (
-          <ValueSpy
-            value={control}
-            didMount={updateOptions}
-            didUpdate={prevControl => {
-              if (!isDeepEqual(control, prevControl)) {
-                updateOptions();
-              }
-            }}
-            willUnmount={removeOptions}
-          />
-        );
+    <MapComponent
+      createOptions={({ maps }: GoogleMapContext) => createControl(maps)}
+      didMount={({ map, options }) => {
+        map.setOptions(createMapOptions(true, options));
       }}
-    </GoogleMapContextConsumer>
+      didUpdate={({ options: prevOptions }, { options: nextOptions, map }) => {
+        if (!isDeepEqual(prevOptions, nextOptions)) {
+          map.setOptions(createMapOptions(true, nextOptions));
+        }
+      }}
+      willUnmount={({ map, options }) => {
+        map.setOptions(createMapOptions(false, options));
+      }}
+    />
   );
 }
