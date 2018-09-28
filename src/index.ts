2ac2b5f6f8d82d36d2ece5fdff36350a654e94b6
Umidbek Karimov <uma.karimov@gmail.com>
2018-09-28 15:57:56 +0500

    feat: Expose public api.

---
+++ src/index.ts
@@ -0,0 +1,31 @@
+export * from "./controls/CustomControl";
+export * from "./controls/FullscreenControl";
+export * from "./controls/MapTypeControl";
+export * from "./controls/RotateControl";
+export * from "./controls/ScaleControl";
+export * from "./controls/StreetViewControl";
+export * from "./controls/ZoomControl";
+
+export * from "./data-layer/DataPolygon";
+
+export * from "./drawing-control/DrawingControl";
+
+export * from "./google-map/GoogleMap";
+export * from "./google-map/GoogleMapContext";
+
+export * from "./info-window/InfoWindow";
+
+export * from "./map-component/MapComponent";
+export * from "./map-component/MapComponentHandlers";
+
+export * from "./marker/Marker";
+export * from "./marker/MarkerIcon";
+export * from "./marker/MarkerSymbol";
+export * from "./marker/MarkerContext";
+
+export * from "./polyline/Polyline";
+
+export * from "./transitions/FitBounds";
+export * from "./transitions/PanBy";
+export * from "./transitions/PanTo";
+export * from "./transitions/PanToBounds";
