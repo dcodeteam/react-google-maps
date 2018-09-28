c8f018cc0eb4c8816ef417ebaacb82f83a5073ee
Umidbek Karimov <uma.karimov@gmail.com>
2018-09-28 18:20:19 +0500

    chore(doczrc): Setup `base` url.

--- doczrc.js
+++ doczrc.js
@@ -1,5 +1,8 @@
 module.exports = {
+  src: "./src",
   dest: "./docs",
+  base: "/react-google-maps/",
+
   typescript: true,
   themeConfig: { mode: "dark" },
   indexHtml: "./config/docz/index.tpl.html",
