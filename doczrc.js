f70199481a32f6e60f846476b953fb9081f79c7e
Umidbek Karimov <uma.karimov@gmail.com>
2018-09-29 11:24:44 +0500

    fix(doczrc.js): Setup `/react-google-maps/`.

--- doczrc.js
+++ doczrc.js
@@ -2,6 +2,7 @@ module.exports = {
   src: "./src",
   dest: "./docs",
   hashRouter: true,
+  base: "/react-google-maps/",
   typescript: true,
   themeConfig: { mode: "dark" },
   indexHtml: "./config/docz/index.tpl.html",
