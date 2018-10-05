ea3ba9694963fe1af5cdaaaf8f327095f558a8db
Umidbek Karimov <uma.karimov@gmail.com>
2018-10-05 11:13:40 +0500

    chore: Get rid of directories in published files.

--- config/rollup/rollup.base.js
+++ config/rollup/rollup.base.js
@@ -3,24 +3,26 @@
 const cleaner = require("rollup-plugin-cleaner");
 const prettier = require("rollup-plugin-prettier");
 const typescript = require("rollup-plugin-typescript2");
+const pkg = require("../../package");
 
 module.exports = function createRollupConfig({ target }) {
+  const externals = [
+    ...Object.keys(pkg.dependencies),
+    ...Object.keys(pkg.devDependencies),
+  ];
+
   return {
     input: "./src/index.ts",
+
     output: {
+      file: `./react-google-maps.${target}.js`,
       format: target.startsWith("es") ? "es" : target,
-      file: `./${target}/index.js`,
     },
+
     external(id) {
-      switch (id) {
-        case "tslib":
-        case "react":
-        case "react-dom":
-          return true;
-        default:
-          return false;
-      }
+      return externals.includes(id);
     },
+
     plugins: [
       cleaner({
         targets: [`./${target}/`, target === "cjs" && "./typings/"].filter(
